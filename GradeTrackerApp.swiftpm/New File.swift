3f954298e549307ccf80a26ee397dd9f997ad78e
Tensilenick <reddy.shrenik@gmail.com>
2026-05-12 12:50:41 -0500

    Add Codable conformance to Task struct

--- GradeTrackerApp.swiftpm/New File.swift
+++ GradeTrackerApp.swiftpm/New File.swift
@@ -1,12 +1,19 @@
-import SwiftUI
-
 import Foundation
 
-struct Task: Identifiable {
-    let id = UUID()
+struct Task: Identifiable, Codable {
+    let id: UUID
     var name: String
     var dueDate: String
     var priority: String
     var completed: Bool
+    
+    init(id: UUID = UUID(), name: String, dueDate: String, priority: String, completed: Bool) {
+        self.id = id
+        self.name = name
+        self.dueDate = dueDate
+        self.priority = priority
+        self.completed = completed
+    }
 }
+
 
